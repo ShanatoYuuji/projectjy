a92bbeb5a8cfa5ac48c4288b3819df253edcf7ce
ShanatoYuuji <c1273099082@qq.com>
2017-04-10 22:08:41 +0900

    邮件测试

--- routes/mail.js
+++ routes/mail.js
@@ -2,6 +2,7 @@ var nodemailer =require('nodemailer');
 var transporter = nodemailer.createTransport({
     //https://github.com/andris9/nodemailer-wellknown#supported-services 支持列表
     service: 'qq',
+  //将发送服务器端口号设置为465或587
     port: 465, // SMTP 端口
     secureConnection: true, // 使用 SSL
     auth: {
--- routes/testmail.js
+++ routes/testmail.js
@@ -2,22 +2,27 @@
  * New node file
  */
 var nodemailer =require('nodemailer');
+var smtpTransport=require('nodemailer-smtp-transport');
+
+
 var transporter = nodemailer.createTransport({
     //https://github.com/andris9/nodemailer-wellknown#supported-services 支持列表
-    service: 'qq',
+    host: "smtp.gmail.com",
+    //将发送服务器端口号设置为465或587
     port: 465, // SMTP 端口
     secureConnection: true, // 使用 SSL
     auth: {
-        user: 'Notice@shakugannoshana.me',
+        user: 'c1273099082@gmail.com',
         //这里密码不是qq密码，是你设置的smtp密码
         //qq邮箱需要生成验证码
-        pass: 'aprhugplomtehbef'
+        pass: 'wchpwdyzcegfwohl'
     }
 });
 
+
 //setup e-mail data with unicode symbols
 var mailOptions = {
-    from: 'Notice@shakugannoshana.me', // 发件地址
+    from: 'c1273099082@gmail.com', // 发件地址
     to: '491128833@qq.com', // 收件列表
     subject: 'Hello sir', // 标题
     //text和html两者只支持一种
